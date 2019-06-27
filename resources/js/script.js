5e34d40b99ee3e875cc3bb69b4e5cc3fa99ed64e
“alexbkennedy96” <akenne36@uncc.edu>
2019-06-27 18:07:52 -0400

    Scroll feature for both buttons in header

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    /* Sticky nav start and end point */ 
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -10,12 +11,13 @@ $(document).ready(function() {
         offset: '60px;'
     })
 
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    })
+
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    })
+
 });
-
-
-// var waypoint = new Waypoint({
-//     element: document.getElementById('basic-waypoint'),
-//     handler: function() {
-//       notify('Basic waypoint triggered')
-//     }
-//   })
