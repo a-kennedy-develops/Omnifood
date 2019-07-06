74fecac2ee14daae4117fbc42b5773c07e1b07f2
“alexbkennedy96” <akenne36@uncc.edu>
2019-07-06 17:10:06 -0400

    Added Mobile/Small screen navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     /* Sticky nav start and end point */ 
-    $('.js--section-features').waypoint(function(direction) {
+$('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
         } else {
@@ -43,6 +43,22 @@ $(document).ready(function() {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '50%'
+    })
+
+    /* Mobile (Small Screen) Navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
     })
 
 });
